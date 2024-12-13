00f9330c07cf31293baf1423da0105215a96f4e1
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-12-13 10:38:31 -0800

    adds solution

--- README.md
+++ README.md
@@ -104,3 +104,29 @@ Produces
 ```py
 0
 ```
+
+## Notes for the Interviewer
+
+### Clarifying Questions
+
+#### Q: How should I handle invalid input? Or letters other than those specified?
+A: You can assume the input will be valid. There is guaranteed to be exactly one 'A' in the grid, and all other strings will be 'T', 'W', or ' '.
+
+#### Q: How should I handle empty input?
+A: You can assume there will be at least one row and column.
+
+#### Q: Will the grid be a square?
+A: The grid is NOT guaranteed to be a square. It may have a different number of rows than columns. However, it will NOT be jagged. It is guaranteed to be a rectangle.
+
+#### Q: Can a pellet pass through a target to hit another? 
+A: Nope.
+
+### Hints
+
+- If your candidate is struggling to form an algorithm, encourage them to explain how they would do it by hand. Afterwards help them to see what data structures might be useful.
+
+- If your candidate is overwhelmed or struggling to get started, encourage them to first write code to find Auberon's starting position. If they get that working then they can move on to seeing what targets can be hit.
+
+- If your candidate is struggling to consider how to check all 4 directions, get them to try just checking one direction (to the right) first. If they get that working, then they can move on to checking the other directions.
+
+- If your candidate is worried about a large number of loops or a large amount of copy/paste, assure them this is OK. The first sample solution has a lot of copy-pasted code!
--- main.py
+++ main.py
@@ -1,6 +1,90 @@
 def hittable_targets(room):
-    # Your implementation here!
-    pass
+    # First, find Auberon by iterating over all
+    # rows and columns
+    for i, row in enumerate(room):
+        for j, item in enumerate(row):
+            if item == 'A':
+                # save starting location in row, column format
+                starting_loc = (i, j)
+
+    # Will hold the number of targets hit
+    hits = 0
+
+    # Have four loops - one to search in each direction
+    # Note that these loops are almost entirely copy/pasted, only
+    # the last line of each while loop is different.
+    # To see a more elegant, but slightly harder to understand solution,
+    # see alternate implementation after the hints
+
+    ### LOOP 1: SEARCH TO THE RIGHT ###
+    # Set r and c to Auberon's location
+    r, c = starting_loc
+    # Step to the right until either
+    # - A wall is hit
+    # - A target is hit
+    # - The edge of the room is exceeded
+    while 0 <= r < len(room) and \
+          0 <= c < len(room[0]):
+        # If a target is hit, increase the hit count by one, and stop
+        # looking in that direction
+        if room[r][c] == 'T':
+            hits += 1
+            break
+        # If a wall is hit, stop looking in that direction
+        if room[r][c] == 'W':
+            break
+        c += 1 # if neither wall nor target is hit, increase column by 1 (move to the right)
+
+
+    ### LOOP 2: SEARCH TO THE LEFT ###
+    # Set r and c to Auberon's location
+    r, c = starting_loc
+    # Search by going to the left
+    while 0 <= r < len(room) and \
+          0 <= c < len(room[0]):
+        # If a target is hit, increase the hit count by one, and stop
+        # looking in that direction
+        if room[r][c] == 'T':
+            hits += 1
+            break
+        # If a wall is hit, stop looking in that direction
+        if room[r][c] == 'W':
+            break
+        c -= 1 # if neither wall nor target is hit, decrease column by 1 (move to the left)
+
+              
+    ### LOOP 3: SEARCH DOWN ###       
+    # Set r and c to Auberon's location
+    r, c = starting_loc
+    while 0 <= r < len(room) and \
+          0 <= c < len(room[0]):
+        # If a target is hit, increase the hit count by one, and stop
+        # looking in that direction
+        if room[r][c] == 'T':
+            hits += 1
+            break
+        # If a wall is hit, stop looking in that direction
+        if room[r][c] == 'W':
+            break
+        r += 1 # if neither wall nor target is hit, increase row by 1 (move down)
+
+
+    ### LOOP4: SEARCH UP ###
+    # Set r and c to Auberon's location
+    r, c = starting_loc
+    while 0 <= r < len(room) and \
+          0 <= c < len(room[0]):
+        # If a target is hit, increase the hit count by one, and stop
+        # looking in that direction
+        if room[r][c] == 'T':
+            hits += 1
+            break
+        # If a wall is hit, stop looking in that direction
+        if room[r][c] == 'W':
+            break
+        r -= 1 # if neither wall nor target is hit, decrease row by 1 (move up)
+
+    return hits
 
 
 room1 = [
